09cf6a8b528c3fe365989fc347d6773a1321123a
akthar <aktharmd221@gmail.com>
2023-12-24 10:08:21 +0530

    dist added

--- src/Shop/PopularPost.jsx
+++ src/Shop/PopularPost.jsx
@@ -4,26 +4,26 @@ import { Link } from 'react-router-dom'
 const title = "Most Popular Posts"
 const postList = [
   {
-    id: 1, imgUrl: '/src/assets/images/blog/10.jpg',
+    id: 1, imgUrl: '/assets/images/blog/10.jpg',
     imgAlt: 'rajibraj91',
     title: 'Poor People Campaign Our Resources',
     date: 'Jun 05,2022',
   },
   { 
-    id: 2, imgUrl: '/src/assets/images/blog/11.jpg',
+    id: 2, imgUrl: '/assets/images/blog/11.jpg',
     imgAlt: 'rajibraj91',
     title: 'Poor Peoples Campaign Our Resources',
     date: 'Jun 05,2022', 
   },
   { 
     id: 3, 
-    imgUrl: '/src/assets/images/blog/12.jpg', 
+    imgUrl: '/assets/images/blog/12.jpg', 
     imgAlt: 'rajibraj91', 
     title: 'Poor Peoples Campaign Our Resources', 
     date: 'Jun 05,2022', 
   },
   { id: 4, 
-    imgUrl: '/src/assets/images/blog/09.jpg', 
+    imgUrl: '/assets/images/blog/09.jpg', 
     imgAlt: 'rajibraj91', 
     title: 'Poor Peoples Campaign Our Resources', 
     date: 'Jun 05,2022', 
